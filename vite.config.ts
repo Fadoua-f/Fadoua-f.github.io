f46bea2ee72c78d05390275468cc3ff5a4a284d0
fadoua elmengoug <elmengougfadwa2003@gmail.com>
2025-11-13 02:21:35 +0100

    YY LOGO

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/", // ✅ très important pour GitHub Pages (déploiement à la racine)
+export default defineConfig(() => ({
+  base: "/", // ✅ nécessaire pour le déploiement à la racine GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
